7501fd7be8cd1cafe844ef453c90293cfbf6a592
Richard Kendall <richard.kendall@gmail.com>
2020-03-21 09:33:44 +1100

    removing catch all error handler for debugging

--- error_handler.py
+++ error_handler.py
@@ -58,6 +58,6 @@ def error_handler(f):
             return exception_to_json_response(err, 401)
         except SystemFailureException as err:
             return exception_to_json_response(err, 502)
-        except Exception as err:
-            return generic_exception_json_response(500)
+        #except Exception as err:
+        #    return generic_exception_json_response(500)
     return error_decorator
