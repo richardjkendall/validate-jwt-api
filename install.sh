9b8619ac9eb0ee1e7894476882d48597b0752cf0
Richard Kendall <richard.kendall@gmail.com>
2020-03-21 14:15:58 +1100

    moving from wget to curl in install

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 # installs python crypto needed by aws lambda
 
-wget https://files.pythonhosted.org/packages/ca/9a/7cece52c46546e214e10811b36b2da52ce1ea7fa203203a629b8dfadad53/cryptography-2.8-cp34-abi3-manylinux2010_x86_64.whl
+curl https://files.pythonhosted.org/packages/ca/9a/7cece52c46546e214e10811b36b2da52ce1ea7fa203203a629b8dfadad53/cryptography-2.8-cp34-abi3-manylinux2010_x86_64.whl --output crypto.whl
 mkdir cypto
-unzip cryptography-2.8-cp34-abi3-manylinux2010_x86_64.whl -d crypto
+unzip crypto.whl -d crypto
 
 rm -Rf output/cryptography
 rm -Rf output/cryptography-2.8.dist-info
