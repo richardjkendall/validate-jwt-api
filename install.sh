2b1adf57d6e90e41b191ae7104174246253478b2
Richard Kendall <richard.kendall@gmail.com>
2020-03-21 14:26:35 +1100

    fixing spelling error

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # installs python crypto needed by aws lambda
 
-curl https://files.pythonhosted.org/packages/ca/9a/7cece52c46546e214e10811b36b2da52ce1ea7fa203203a629b8dfadad53/cryptography-2.8-cp34-abi3-manylinux2010_x86_64.whl --output crypto.whl
-mkdir cypto
+curl https://files.pythonhosted.org/packages/45/73/d18a8884de8bffdcda475728008b5b13be7fbef40a2acc81a0d5d524175d/cryptography-2.8-cp34-abi3-manylinux1_x86_64.whl --output crypto.whl
+if [ -d crypto ]; then rm -Rf crypto; fi
 unzip crypto.whl -d crypto
 
 rm -Rf output/cryptography
