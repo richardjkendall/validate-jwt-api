59a694f0bb9425d1912a42209274deb1b4bea681
Richard Kendall <richard.kendall@gmail.com>
2020-03-21 14:36:45 +1100

    adding cffi module for install

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ rm -Rf output/cryptography-2.8.dist-info
 
 mv crypto/cryptography output
 mv crypto/cryptography-2.8.dist-info output
+
+curl https://files.pythonhosted.org/packages/49/de/1ab40dd857172da4accad610c25badfdfc23ec7e1bbcc6f9212cfcb3419d/cffi-1.14.0-cp38-cp38-manylinux1_x86_64.whl --output cffi.zip
+if [ -d cffi ]; then rm -R cffi; fi
+unzip cffi.whl -d cffi
+rm output/_cffi_backend*
+cp cffi/_cffi_backend.cpython-38-x86_64-linux-gnu.so output/
